94ca486d6c9716b8c3e8963a2c9be8cb4ead33eb
sjc5182 <shiguangchen1987@gmail.com>
2019-06-07 02:11:04 -0700

    tree submit

--- bst.js
+++ bst.js
@@ -2,14 +2,47 @@ var BSTNode = /** @class */ (function () {
     // constructor(public value: number) // this is same as writtng like "value: number and constructor gets pram value and assign it back to object body"
     function BSTNode(value) {
         this.value = value;
+        this.left = undefined;
+        this.right = undefined;
     }
     return BSTNode;
 }());
 var BinarySearchTree = /** @class */ (function () {
-    function BinarySearchTree(root) {
-        this.root = root;
+    function BinarySearchTree() {
+        this.root = undefined;
     }
+    BinarySearchTree.prototype.insert = function (value) {
+        var newNode = new BSTNode(value);
+        if (this.root == undefined) {
+            this.root = newNode;
+            return this;
+        }
+        else {
+            var current = this.root;
+            while (true) {
+                if (current.value > value) {
+                    if (current.left == undefined) {
+                        current.left = newNode;
+                        return this;
+                    }
+                    current = current.left;
+                }
+                else if (current.value < value) {
+                    if (current.right == undefined) {
+                        current.right = newNode;
+                        return this;
+                    }
+                    current = current.right;
+                }
+            }
+        }
+    };
     return BinarySearchTree;
 }());
-var bst = new BSTNode(3);
+var bst = new BinarySearchTree();
+bst.insert(4);
+bst.insert(2);
+bst.insert(5);
+bst.insert(1);
+bst.insert(9);
 console.log(bst);
--- bst.ts
+++ bst.ts
@@ -1,22 +1,53 @@
 class BSTNode {
-  value: number;
-  left: null;
-  right: null
+  public value: number;
+  public left: BSTNode;
+  public right: BSTNode
   // constructor(public value: number) // this is same as writtng like "value: number and constructor gets pram value and assign it back to object body"
   constructor(value: number){
-    this.value = value
+    this.value = value;
+    this.left = undefined;
+    this.right = undefined
   }
 }
 
 class BinarySearchTree {
-  constructor(public root: null){
+  root: BSTNode;
+  constructor(){
+    this.root = undefined
   }
-
-  public insert(value: number){
+  
+  public insert(value: number): this{
     var newNode = new BSTNode(value)
-    
+    if(this.root == undefined){
+      this.root = newNode
+      return this
+    }else{
+      var current: BSTNode = this.root
+      while(true){
+        if(current.value>value){
+          if(current.left == undefined){
+            current.left = newNode
+            return this
+          }
+          current = current.left 
+        }else if(current.value<value){
+          if(current.right == undefined){
+            current.right = newNode
+            return this
+          }
+          current = current.right
+        }
+      }
+    }
   }
 }
 
-const bst = new BSTNode(3)
+const bst = new BinarySearchTree()
+
+bst.insert(4)
+bst.insert(2)
+bst.insert(5)
+bst.insert(1)
+bst.insert(9)
+
 console.log(bst)
